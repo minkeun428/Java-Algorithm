86b3e239db3adff290d83559da452426f847c9fe
minkeun <minkeun428@gmail.com>
2021-06-02 20:26:05 +0900

    1929 - 소수 구하기

---
+++ src/baekjoon/FindPrimeNumber.java
@@ -0,0 +1,45 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class FindPrimeNumber {
+    static boolean[] prime;
+
+    public static void main(String[] args) throws IOException {
+        //M이상 N이하의 소수가 하나 이상 있는 입력만 주어짐
+        //한 줄에 하나씩, 증가하는 순서대로 소수를 출력
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        prime = new boolean[N + 1];
+        find_prime();
+
+        for(int i = M; i <= N; i++) {
+            //false = 소수
+            if(!prime[i]) sb.append(i).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    static void find_prime() {
+        prime[0] = prime[1] = true;
+
+        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if(prime[i]) continue;
+
+            for(int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
+    }
+
+}
