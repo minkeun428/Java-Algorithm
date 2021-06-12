afe83bf35c651ebfc10a9efa5c5f87480325027c
minkeun <minkeun428@gmail.com>
2021-06-12 16:50:07 +0900

    1단계 - 짝수와 홀수

---
+++ src/programmers1/EvenAndOdd.java
@@ -0,0 +1,10 @@
+package programmers1;
+
+public class EvenAndOdd {
+    public String solution(int num) {
+
+        String answer = num % 2 == 0 ? "Even" : "Odd";
+
+        return answer;
+    }
+}
