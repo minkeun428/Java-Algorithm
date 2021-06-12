902ed5257814834705247f5924e1cd24756e4691
minkeun <minkeun428@gmail.com>
2021-06-12 14:51:16 +0900

    1단계 - 직사각형 별찍기

---
+++ src/programmers1/PutStars.java
@@ -0,0 +1,22 @@
+package programmers1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PutStars {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<b; i++) {
+            for(int j=0; j<a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
