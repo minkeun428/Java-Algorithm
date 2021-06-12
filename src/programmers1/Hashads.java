c3e122ed1fd52ee00dabccdd839064181977c6ea
minkeun <minkeun428@gmail.com>
2021-06-12 15:53:31 +0900

    1단계 - 하샤드 수 수정

--- src/programmers1/Hashads.java
+++ src/programmers1/Hashads.java
@@ -4,12 +4,21 @@ public class Hashads {
     class Solution {
         public boolean solution(int x) {
             boolean answer = true;
+            int sum = 0;
+            int chk = x;
 
-            int sib = x / 10;
-            int il = x % 10;
-            int rtn = sib + il;
+            while(chk != 0) {
+                sum += chk % 10;
+                chk /= 10;
+            }
 
-            if(x % rtn != 0) {
+            // 밑의 코드처럼 하면 chk에 마지막 값들 못 넣고 나가버림
+            // while(chk / 10 != 0) {
+            //     sum += chk % 10;
+            //     chk /= 10;
+            // }
+
+            if(x % sum != 0) {
                 answer = false;
             }
 
