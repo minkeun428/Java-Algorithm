ee9a5316d4b9a64f2d10c1280c53120eb2179445
minkeun <minkeun428@gmail.com>
2021-06-14 20:50:20 +0900

    1단계 - 자릿수 더하기

---
+++ src/programmers1/AddDigits.java
@@ -0,0 +1,16 @@
+package programmers1;
+
+public class AddDigits {
+
+    public int solution(int n) {
+        int answer = 0;
+
+        while(n > 0) {
+            answer += n % 10;
+            n = n / 10;
+        }
+
+        return answer;
+    }
+
+}
