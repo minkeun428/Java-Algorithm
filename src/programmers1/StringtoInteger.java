0e5ae4d7edb037761e4614d4cc74f7ab3b22250e
minkeun <minkeun428@gmail.com>
2021-06-15 18:49:41 +0900

    1단계 - 문자열을 정수로 바꾸기

---
+++ src/programmers1/StringtoInteger.java
@@ -0,0 +1,11 @@
+package programmers1;
+
+public class StringtoInteger {
+    public int solution(String s) {
+        int answer = 0;
+
+        answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
