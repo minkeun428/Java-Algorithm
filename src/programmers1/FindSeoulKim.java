af773c8d4c9cdd63c58a6dd37111b445c6bb072f
minkeun <minkeun428@gmail.com>
2021-06-16 11:49:55 +0900

    1단계 - 서울에서 김서방 찾기

---
+++ src/programmers1/FindSeoulKim.java
@@ -0,0 +1,16 @@
+package programmers1;
+
+public class FindSeoulKim {
+    public String solution(String[] seoul) {
+        String answer = "";
+
+        for(int i = 0; i < seoul.length; i++) {
+            if(seoul[i].equals("Kim")) {
+                answer = "김서방은 " + i + "에 있다";
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
