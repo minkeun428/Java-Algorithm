19624dea762c02d16d5b8639265f45c75ea64ae3
minkeun <minkeun428@gmail.com>
2021-06-17 15:45:37 +0900

    1단계 - 두 정수 사이의 합

---
+++ src/programmers1/TwoIntSum.java
@@ -0,0 +1,21 @@
+package programmers1;
+
+public class TwoIntSum {
+
+    public long solution(int a, int b) {
+        long answer = 0;
+        if(a==b){
+            answer = a;
+        }else if(a<b){
+            for(int i=a; i<=b; i++){
+                answer += i;
+            }
+        }else if(a>b){
+            for(int i=b; i<=a; i++){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+
+}
