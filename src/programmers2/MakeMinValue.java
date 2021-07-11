f6b258dd2f36afb3de8568a2792b873546bedd3b
minkeun <minkeun428@gmail.com>
2021-07-11 23:23:10 +0900

    최솟값 만들기 : 내림차순 안될 때

--- src/programmers2/MakeMinValue.java
+++ src/programmers2/MakeMinValue.java
@@ -7,6 +7,9 @@ public class MakeMinValue {
         Arrays.sort(A);
         Arrays.sort(B);
         int answer = 0;
+
+        // 둘 다 오름차순으로 정렬 후,
+        // B는 뒤 인덱스부터 곱해야 최소가 됨.
         for (int i = 0; i < A.length; i++) {
             answer += A[i] * B[B.length - 1 - i];
         }
