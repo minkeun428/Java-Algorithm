3a7481cf4881eaa3c4c39792ed5ba088cf39af66
minkeun <minkeun428@gmail.com>
2021-08-05 17:20:21 +0900

    링크드리스트 : 뒤집기

---
+++ src/leetcode/easy/ReverseLinkedList.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+
+        //서로 긴밀하게 연결되어 있기 때문에
+        //코드는 보기 쉬움
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
